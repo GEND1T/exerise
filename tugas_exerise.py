22ae2b68f9e3bc0821c8c954ad60c3a44c7fd854
GEND1T <akunumum1606@gmail.com>
2024-11-13 04:51:30 +0700

    Add files via upload

---
+++ tugas_exerise.py
@@ -0,0 +1,13 @@
+# Resensi Novel Laskar Pelangi
+judul_buku = "Laskar_pelangi"
+tahun_terbit = 2005
+harga = 55000
+penulis = "andrea_hirata"
+ketersediaan_di_perpustakaan = "ada"
+
+data_buku = [(judul_buku),
+(tahun_terbit),
+(harga),
+(penulis),
+(ketersediaan_di_perpustakaan)]
+print(data_buku)
